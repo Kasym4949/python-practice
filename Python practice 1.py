ae1b367b193cc98449070e8f33d1b59689c66b05
Kasym4949 <kanatulykasymhan0@gmail.com>
2025-09-09 07:03:12 +0500

    "First basic calculator".
    I added a simple calculator with input for first and second numbers and basic arithmetic operations.

---
+++ Python practice 1.py
@@ -0,0 +1,19 @@
+First_num=int(input("Enter first number: "))
+Second_num=int(input("Enter second number: "))
+Sum=First_num+Second_num
+num1=First_num+Second_num
+num2=First_num-Second_num
+num3=First_num*Second_num
+num4=First_num/Second_num
+num5=First_num//Second_num
+num6=First_num%Second_num
+num7=First_num**Second_num
+print(First_num)
+print(Second_num)
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+print(num6)
+print(num7)
