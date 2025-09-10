df540bd46c0cbc8e56f8ecc0a11c1b8eb2e040bd
Kasym4949 <kanatulykasymhan0@gmail.com>
2025-09-10 06:59:24 +0500

    Conditional statements
    I practiced two tasks: checking odd or even numbers, and checking whether a number is positive, negative, or zero.

---
+++ Python practice 2.1.py
@@ -0,0 +1,5 @@
+user_number=int(input('Enter a number: '))
+if user_number%2==0:
+    print("This is an even number", user_number)
+else:
+    print("This is an odd number")
---
+++ Python practice 2.2.py
@@ -0,0 +1,7 @@
+user_num=int(input('Enter your number: '))
+if user_num>0:
+    print(f"Your number {user_num} is positive")
+elif user_num<0:
+    print(f"Your number {user_num} is negative")
+else:
+    print(f"Your number {user_num} is zero")
